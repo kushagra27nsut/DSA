d87407ba6c2d3c539574e4a8875207828af4b57f
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-06 04:01:29 +0530

    Time: 29 ms (81.68%) | Memory: 82.8 MB (79.62%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n = nums.size() ;
-        unordered_set<int> st;
-        for (int i = 0; i < n; ++i) {
-            if(st.find(nums[i]) != st.end()) return true ;
-            st.insert(nums[i]) ;
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
         }
-        return false ;
+        return false;
     }
 };
