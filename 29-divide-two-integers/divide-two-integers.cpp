5c60f13fe6851efc9f461a58ba04175124487a59
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-06 20:40:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (38.63%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -4,16 +4,20 @@ public:
         if(dividend == divisor) return 1;
         if(dividend == INT_MIN && divisor == -1) return INT_MAX;
         if(divisor == 1) return dividend;
-        long long a = dividend ;
-        long long b = divisor ;
+        long long n= dividend ;
+        long long d = divisor ;
         bool ispositive = true ;
-        if (a>=0 && b<0) ispositive = false ;
-        if (a<0 && b>0) ispositive = false ;
-        a = abs(a) , b = abs(b) ;
-        long long sum = 0 , ans = 0 ;
-        while ( sum + b <= a ){
-            ans++ ;
-            sum += b ;
+        if (n>=0 && d<0) ispositive = false ;
+        if (n<0 && d>0) ispositive = false ;
+        n= abs(n) , d = abs(d) ;
+        int ans = 0;
+        while ( n >= d ){
+            int count = 0 ;
+            while (n >= (d<<(count + 1))){
+                count++ ;
+            }
+            ans += 1<<count ;
+            n -= (d<<count) ;
         }
         if (ans > INT_MAX && ispositive == false) return INT_MIN ;
         if (ans > INT_MAX && ispositive == true ) return INT_MAX ;
