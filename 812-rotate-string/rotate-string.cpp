b2838e6ac9be556a421a1a095938b380514daf0f
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-08 04:37:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (78.00%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     bool rotateString(string& s, string goal) {
-        if (s == goal) return true;
-        int n = s.size() ;
-        for(int i = 0 ; i < n ; i ++ ){
-            char o = s[0] ;
-            s.erase(0,1); 
-            s.push_back(o) ;
-            if(s == goal) return true ;
-        }
-        return false ;
+        return s.size() == goal.size() && (s+s).find(goal) != string::npos;
     }
 };
