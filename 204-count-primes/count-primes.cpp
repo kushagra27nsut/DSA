d908ce3d9aadbc3b60a16078996a3f30b3670057
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-09 03:54:05 +0530

    Time: 81 ms (98.44%) | Memory: 12.8 MB (25.36%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -3,16 +3,16 @@ public:
     int countPrimes(int n) {
         if (n <= 2) return 0;
         vector<bool> prime(n,true) ;
-        int count = 0 ;
+        int count = 1 ;
         prime[0] = prime[1] = false ;
-        for(int i= 2 ; i*i <= n ; i++){
+        for(int i= 3 ; i*i <= n ; i+=2){
             if(prime[i]){
-                for(int j = i*i ; j < n ; j+=i){
+                for(int j = i*i ; j < n ; j+=i*2){
                     prime[j] = false ;
                 }
             }
         }
-        for(int i = 2 ; i < n ; i++ ){
+        for(int i = 3 ; i < n ; i+=2 ){
             if(prime[i]){
                 count++ ;
             }
