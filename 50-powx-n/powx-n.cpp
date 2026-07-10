ceec7fef0e76de0c1da9dbfe97327b3cf030e532
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-10 14:14:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.33%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -3,10 +3,11 @@ public:
     double power(double x , long long n ){
         if(n == 0) return 1.0 ;
         if(n == 1) return x ;
+        double half = power(x,n/2) ;
         if(n%2 == 0){
-            return power(x*x  , n/2);
+            return half*half ;
         }
-        return x*power(x,n-1) ;
+        return x*half*half ;
     }
     double myPow(double x, int n) {
         long long N = n ;
