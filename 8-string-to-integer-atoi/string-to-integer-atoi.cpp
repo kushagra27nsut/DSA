90da06efa20df411f799d1d10855c7b35b674ad5
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-11 02:33:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (81.96%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int com(string s , int i , long long n , int sign){
-        if(i >= s.size() || !isdigit(s[i]) ){
-            return int(sign*n) ;
-        }
-        n = n*10 + (s[i] - '0') ;
-        if( sign*n > INT_MAX) return INT_MAX ;
-        if(sign*n < INT_MIN ) return INT_MIN ;
-        return com(s,i+1,n,sign) ;
-    }
     int myAtoi(string s) {
-        int i = 0 ;
-        while(s[i] == ' ') i++ ;
-        int sign = 1 ; 
-        if( i < s.size() && ( s[i] == '+' || s[i] == '-')){
-            sign = (s[i] == '-') ? -1 : 1 ;
-            i++ ;
+        long long res=0;
+        int sign=1,i=0;
+        while ( i < s.size() && s[i] == ' ')
+            i++;
+        if(i == s.size() ) return 0;
+        if( s[i] == '-'){
+        sign = -1;
+        i++;
         }
-        return com(s , i , 0 , sign) ;
+        else if (s[i]=='+')
+        i++;
+        while ( i < s.size() && isdigit(s[i])){
+            res = res*10 + (s[i]-'0');
+            if (sign * res > INT_MAX) return INT_MAX;
+            if (sign * res < INT_MIN) return INT_MIN;
+            i++;
+        }
+        return (int)sign*res;
     }
 };
