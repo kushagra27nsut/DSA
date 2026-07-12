5533835ed9983973562cdbe4c0672a2754ac2810
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-12 22:24:39 +0530

    Time: 4 ms (94.15%) | Memory: 9.3 MB (92.20%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -5,6 +5,7 @@ public:
         if( n <= 1 ) return s ;
         int yaaart = 0 , maxlen = 1 ;
         for(int i = 0 ; i < n ; i++ ){
+            if ((n - i) * 2 <= maxlen) break;
             int left = i , right = i ;
             while ( left >= 0 && right < n && s[left] == s[right]){
                 if(right - left + 1 > maxlen ){
