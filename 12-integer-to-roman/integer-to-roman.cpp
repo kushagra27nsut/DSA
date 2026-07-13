b6f6d7f8779584a02fab2ded000b26d8d7fdbc7b
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-13 06:14:20 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (72.96%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        unordered_map<int,char> mp = {
-            {1000,'M'} , {500,'D'} , {100,'C'} , {50,'L'} ,{10,'X'} , {5,'V'} , {1,'I'} 
-        } ;
-        int m = 1 ;
+        char one[] = {'I','X','C','M'} ;
+        char five[] = {'V','L','D'} ;
+        int m = 0 ;
         string s = "" ;
         while (num){
             int n = num%10 ;
             if( n == 9 ){
-                s = string(1,mp[m])+ string(1,mp[m*10]) + s ;
+                s = string(1,one[m])+ string(1,one[m+1]) + s ;
             }
             else if ( n == 4){
-                s = string(1,mp[m]) + string(1,mp[5*m]) + s ;
+                s = string(1,one[m]) + string(1,five[m]) + s ;
             }
             else if (n >= 5){
-                s = string(1,mp[m*5]) + string(n-5,mp[m]) + s ;
+                s = string(1,five[m]) + string(n-5,one[m]) + s ;
             }
-            else if (n > 0){
-                s = string(n,mp[m]) + s ;
+            else {
+                s = string(n,one[m]) + s ;
             }
-            m = m*10 ;
+            m++ ;
             num /= 10 ;
         }
         return s ;
