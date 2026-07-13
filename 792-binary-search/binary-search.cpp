cda6a32c10f9eda4de569cc628059aca985e2765
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-13 22:14:03 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (46.38%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size() ;
-        int l = 0 , r = n - 1 ;
+        int l = 0 , r = nums.size() - 1 ;
         while (l<=r){
             int mid = (l+r)/2 ;
             if(nums[mid] == target) return mid ;
