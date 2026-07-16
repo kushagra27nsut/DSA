ec90252937c37466d4c766bada3a400b686ca1d0
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-16 22:42:45 +0530

    Time: 14 ms (36.05%) | Memory: 35.2 MB (19.65%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin() , weights.end()) ;
+        int high = 0 ;
+        for(int n : weights){
+            high += n ;
+        }
+        while(low <= high ){
+            int mid = low + (high - low )/2 ;
+            int count = 0 , tmp = 1 ;
+            for(int num : weights){
+                if(count + num <= mid ){
+                    count += num ;
+                }
+                else{
+                    tmp++ ;
+                    count = num ;
+                }
+            }
+            if (tmp > days){
+                low = mid + 1 ;
+            }
+            else {
+                high = mid - 1 ;
+            }
+        }
+        return low ;
+    }
+};
