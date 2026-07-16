84da7eccc7ccfe175ddb7b36c0a29fd4d0f3a912
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-16 21:54:48 +0530

    Time: 5 ms (72.62%) | Memory: 26 MB (80.73%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low = 1 ;
+        int high = *max_element(nums.begin(),nums.end()) ;
+        while(low <= high ){
+            int mid = low + (high - low)/2 ;
+            int sum = 0 ;
+            for(int num : nums){
+                sum += (num + mid - 1 )/mid ;
+                if( sum > threshold) break ;
+            }
+            if (sum > threshold) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1 ;
+            }
+        }
+        return low ;
+    }
+};
