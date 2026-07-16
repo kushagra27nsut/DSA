d4d8313120e1d6de5e89dbcebd6425005262bf07
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-16 14:33:44 +0530

    Time: 6 ms (75.40%) | Memory: 23 MB (15.49%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxel = 0 ;
+        for(int num : piles) {
+            if(num > maxel){
+                maxel = num ;
+            }
+        }
+        int low = 1 , high = maxel ;
+        while(low <= high){
+            int mid = (low + high)/2 ;
+            int hours = 0 ;
+            for(int num : piles){
+                hours += ( num + mid - 1)/ mid ;
+                if(hours > h) break;
+            }
+            if( hours > h ){
+                low = mid + 1 ;
+            }
+            else{
+                high = mid - 1 ;
+            }
+        }
+        return low ;
+    }
+};
