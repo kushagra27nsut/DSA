8f5cfb849e0ef861c15ca46a1a36d90f53389f80
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-07-28 21:04:12 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (29.33%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
-    bool help(int target, vector<int>& nums, int len) {
-        int sum = 0;
-
-        for (int i = 0; i < len; i++)
-            sum += nums[i];
-
-        if (sum >= target)
-            return true;
-
-        for (int i = len; i < nums.size(); i++) {
-            sum += nums[i];
-            sum -= nums[i - len];
-
-            if (sum >= target)
-                return true;
-        }
-
-        return false;
-    }
     int minSubArrayLen(int target, vector<int>& nums) {
-        int n = nums.size() ;
-        int low = 1 , high = n ;
-        while(low <= high){
-            int mid = low + (high - low)/2 ;
-            if (help(target , nums, mid)){
-                high = mid - 1 ;
+        int i = 0 , j = 0 , n = nums.size() ;
+        int sum = 0 , m = INT_MAX ;
+        while( j < n ){
+            sum += nums[j] ;
+            while( sum >= target){
+                sum -= nums[i] ;
+                m = min(m,j-i+1) ;
+                i++ ;
             }
-            else{
-                low = mid + 1 ;
-            }
+            j++ ;
         }
-        if (low == n+1 ) return 0 ;
-        return low ;
+        if(m == INT_MAX){
+            return 0 ;
+        }
+        return m ;
     }
 };
