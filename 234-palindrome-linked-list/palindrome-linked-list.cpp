e890f7b69c4fec11367c5c62dffe5ec612b57b80
KUSHAGRA SHARMA <kushagra.sharma.ug25@nsut.ac.in>
2026-08-16 23:36:57 +0530

    Time: 3 ms (58.47%) | Memory: 117.9 MB (94.05%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head ;
+        ListNode* fast = head ;
+        while(fast!= nullptr && fast->next != nullptr){
+            slow = slow->next ;
+            fast = fast->next->next ;
+        }
+        ListNode* curr = slow ;
+        ListNode* prev = nullptr ;
+        while(curr){
+            ListNode* nxt = curr->next ;
+            curr->next = prev ;
+            prev = curr ;
+            curr = nxt ;
+        }
+        ListNode* pvt = head ;
+        while(prev){
+            if (pvt->val != prev->val){
+                return false ;
+            }
+            pvt = pvt->next ;
+            prev = prev->next ;
+        }
+        return true ;
+    }
+};
